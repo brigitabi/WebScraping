541d0a769c670c0b619de01825c4350baf5fd32d
Brigita Bizjak <brigita.bizjak05@gmail.com>
2023-07-04 14:34:27 +0200

    adding html styling and more logic

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000
+const PORT = 8000;
 
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -8,27 +8,69 @@ const path = require('path');
 const app = express();
 const router = express.Router();
 
-const url = 'https://www.theguardian.com/technology/chatgpt'
+const url = 'https://www.theguardian.com/technology/chatgpt';
 
 axios(url)
-    .then(response => { 
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
 
+    $('.fc-item__content', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find('a').attr('href');
+      const date = $(this).closest('.fc-item').find('.fc-item__timestamp').text();
+      
+      if (title.includes('ChatGPT')) {
+      articles.push({
+        title,
+        url,
+        date
+      });
+    }
+    });
 
-        $('.fc-item__title', html).each(function() {
-            const title = $(this).text()
-            const url = $(this).find('a').attr('href')
-            const date = $(this).closest('.fc-item').find('.fc-item__timestamp').text();
+    // Sorting the articles array in descending order based on the date
+    articles.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-            articles.push({
-                title, 
-                url,
-                date
-            })
-        })
-        console.log(articles)
-    }).catch(err => console.log(err))
+    // Generating an HTML response
+    const generateHTMLResponse = () => `
+      <html>
+        <head>
+          <link rel="stylesheet" href="/styles.css">
+        </head>
+        <body>
+          <h1>Hey there! Here are your scraped articles📄</h1>
+          <div class="articles">
+            ${articles.map(article => `
+              <div class="article">
+                <div class="title">${article.title}</div>
+                <div class="url">
+                  <a href="${article.url}" target="_blank">${article.url}</a>
+                </div>
+                <div class="date">${article.date}</div>
+              </div>
+            `).join('')}
+          </div>
+        </body>
+      </html>
+    `;
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+    // Defining a route on the router to handle the root URL
+    router.get('/', (req, res) => {
+      res.send(generateHTMLResponse());
+    });
+
+    // Registering the router with the app
+    app.use('/', router);
+
+    // Serving the static files (CSS file)
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Starting the server and listen on the specified port
+    app.listen(PORT, () => {
+      console.log(`Server running on PORT ${PORT}`);
+      console.log(`Open http://localhost:${PORT}/ to see the scraped data.`);
+    });
+  })
+  .catch(err => console.log(err));
